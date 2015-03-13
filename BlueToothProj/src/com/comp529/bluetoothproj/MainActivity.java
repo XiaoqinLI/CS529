8818827a61a92db5af5bcc7d506716988ed74258
XiaoqinLI <daybreaklee@utexas.edu>
2015-03-12 21:15:31 -0500

    added BluetoothAdapter, mainactivity auto checks if bluetooth is on

--- BlueToothProj/src/com/comp529/bluetoothproj/MainActivity.java
+++ BlueToothProj/src/com/comp529/bluetoothproj/MainActivity.java
@@ -1,16 +1,31 @@
 package com.comp529.bluetoothproj;
 
 import android.support.v7.app.ActionBarActivity;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends ActionBarActivity {
-
+	
+	private static final int REQUEST_ENABLE_BT = 1;
+			
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+		if (mBluetoothAdapter == null) {
+		    // Device does not support Bluetooth
+		}
+		
+		if (!mBluetoothAdapter.isEnabled()) {
+		    Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+		    startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+		}
+		
 	}
 
 	@Override
