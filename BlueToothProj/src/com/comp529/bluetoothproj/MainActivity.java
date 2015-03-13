58624778403b64292d922a0a4a8589fc6f10f725
XiaoqinLI <daybreaklee@utexas.edu>
2015-03-12 23:36:58 -0500

    fixed a bug in pairadapter and tested it with fake data

--- BlueToothProj/src/com/comp529/bluetoothproj/MainActivity.java
+++ BlueToothProj/src/com/comp529/bluetoothproj/MainActivity.java
@@ -1,28 +1,61 @@
 package com.comp529.bluetoothproj;
 
+import java.util.ArrayList;
+import java.util.Set;
+
+import com.comp529.bluetoothproj.adapter.PairAdapter;
+import com.comp529.bluetoothproj.model.Pair;
+
 import android.support.v7.app.ActionBarActivity;
+import android.app.ListActivity;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.AdapterView;
 import android.widget.Toast;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ListActivity {
 	
 	private static final int REQUEST_ENABLE_BT = 1;
-	
+	private static ArrayList<Pair> arrayOfPairs = new ArrayList<Pair>();
+	PairAdapter pairAdapter;
 			
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
+		pairAdapter = new PairAdapter(this, arrayOfPairs);
+		// add a fake testing data
+		pairAdapter.add(new Pair("foo", "woo"));
+		
+		final ListView pairListView = getListView();
+		
+		pairListView.setAdapter(pairAdapter);
+		
+		LayoutInflater inflater = getLayoutInflater();
+        ViewGroup header = (ViewGroup) inflater.inflate(R.layout.item_pair, pairListView, false);
+		
 		// get the default bluetooth adapter
 		BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+		// turn on BlueTooth, if not supported, then shut the activity down.
 		activatingBlueTooth(mBluetoothAdapter);
-		
-		
+		// check if there are devices already paired and known.
+		Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+		// If there are paired devices
+		if (pairedDevices.size() > 0) {
+		    // Loop through paired devices
+		    for (BluetoothDevice device : pairedDevices) {
+		        // Add the name and address to an array adapter to show in a ListView
+		    	pairAdapter.add(new Pair(device.getName() , device.getAddress()));
+		    }
+		}
 	}
 
 	@Override
@@ -48,6 +81,14 @@ public class MainActivity extends ActionBarActivity {
 		
 		if (mBluetoothAdapter == null) {
 			Toast.makeText(MainActivity.this, "Device does not support Bluetooth", Toast.LENGTH_LONG).show();
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				//e.printStackTrace();
+				Thread.currentThread().interrupt();
+			}
+			this.finish();
 		}
 		
 		else if (!mBluetoothAdapter.isEnabled()) {
--- BlueToothProj/src/com/comp529/bluetoothproj/adapter/PairAdapter.java
+++ BlueToothProj/src/com/comp529/bluetoothproj/adapter/PairAdapter.java
@@ -26,7 +26,7 @@ public class PairAdapter extends ArrayAdapter<Pair>{
        Pair pair = getItem(position);    
        // Check if an existing view is being reused, otherwise inflate the view
        if (convertView == null) {
-          convertView = LayoutInflater.from(getContext()).inflate(R.layout.activity_main, parent, false);
+          convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_pair, parent, false);
        }
        // Lookup view for data population
        TextView pairNameTV = (TextView) convertView.findViewById(R.id.pair_name);
