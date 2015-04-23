71e71fff98dd58f2cb0c716e23d39bb48caccf31
Yiding Xu <Yiding1@XuYidings-MacBook-Pro.local>
2015-04-23 01:19:04 -0500

    final version with comments from Yiding

--- BlueToothNetwork/src/com/example/bluetoothnetwork/MiddleClass.java
+++ BlueToothNetwork/src/com/example/bluetoothnetwork/MiddleClass.java
@@ -30,7 +30,7 @@ abstract public class MiddleClass extends LowerClass{
 
 	private User thisUser;
 	
-	private Map<User, Set<User>> userGraph;// = new HashMap();
+	private Map<User, Set<User>> userGraph;
 	
 	private Set<UUID> uuidSet = new HashSet<UUID>();
 	
@@ -73,27 +73,25 @@ abstract public class MiddleClass extends LowerClass{
 	abstract void onMessage(Message message); //called inside onReceivingData getAllUsers();
 
 	
-	private User generateUser(String MacAddress){
-		return new User(mChatService.macToName.get(MacAddress), macToLong(MacAddress));
-	}
 	
 	
+	// Send Msg, if the receiver is reachable, broadcast
 	Boolean sendMessage(User user, Message message){ 
 		Data data = new TextData(message.getSender(),message.getReceiver(), message.getMessage());
 		
-		//if (BFS().contains(message.getReceiver())){
+		if (BFS().contains(message.getReceiver())){
 			Set<String> availableDevices = this.getAllConnectedDevices();
 			for (String s: availableDevices)
 				try {
 					this.sendData( s, serialize(data));
 				} catch (IOException e) {
-					// TODO Auto-generated catch block
+				
 					e.printStackTrace();
 				}
 			return true;
 			
-		//}else
-			//return false;
+		}else
+			return false;
 	
 		
 			
@@ -109,11 +107,33 @@ abstract public class MiddleClass extends LowerClass{
 	
 	
 	@Override
-	// step 1: to broadcast newNeighbor msg, which is to urge anyone to broadcast there own topology
-	// step 2: broadcast its own direct neighbors.
+	// step 1: add the link into its current topology, test whether a new neighbor coming
+	// step 2: to broadcast newNeighbor msg, which is to urge anyone to broadcast their own topology
+	// step 3: broadcast its own updated direct neighbors.
 	void onNewNeighbor(User user) {
+		// add this edge in the graph
+		
+		// thisUser -> user
+		List<User> current = BFS();
+		if (!current.contains(user)){
+			current.clear();
+			current.add(user);
+			this.onNewUser(current);
+		}
 		
 		this.userGraph.get(thisUser).add(user);
+		
+		// user -> thisUser
+		if (this.userGraph.containsKey(user)){
+			this.userGraph.get(user).add(thisUser);
+			
+		}else{
+			Set<User> newSet = new HashSet<User>();
+			newSet.add(thisUser);
+			this.userGraph.put(user, newSet);
+		}
+		
+		// broadcast ADDNEIGHBORdata
 	
 		Data neighborData = new AddNeighborData(this.thisUser, null, user);
 		
@@ -125,7 +145,7 @@ abstract public class MiddleClass extends LowerClass{
 				e.printStackTrace();
 			}
 		
-		
+		// broadcast updated topology
 		Data thisTopoData = new TopologyData(this.thisUser, null, this.userGraph.get(thisUser));
 		for (String s: availableDevices)
 			try {
@@ -134,21 +154,21 @@ abstract public class MiddleClass extends LowerClass{
 				e.printStackTrace();
 			}
 		
-//		List<User> result = new ArrayList<User>();
-//		result.add(user);
-//		
-//		this.onNewUser(result);
+
 		
 	}
 
 	@Override
+	// step 1: break the edge in the topology
+	// step 2: broadcast removeDataMsg
+	// step 3: check the current status
 	void onNeighborLeaving(User user) {
 		
-		// cut down the link within this user and the coming user in the usergraph
-		// then broadcast the userleaving msg
-		// then perform bst to determine which nodes are leaving.
-		this.userGraph.get(thisUser).remove(user);
 		
+		
+		
+		this.userGraph.get(thisUser).remove(user);
+		this.userGraph.get(user).remove(thisUser);
 		
 		
 		Data neighborData = new RemoveNeighborData(this.thisUser, null, user);
@@ -157,7 +177,6 @@ abstract public class MiddleClass extends LowerClass{
 			try {
 				this.sendData( s, serialize(neighborData));
 			} catch (IOException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		
@@ -173,10 +192,10 @@ abstract public class MiddleClass extends LowerClass{
 		
 	}
 	
-	
+	// this is to return the current reachable user sets.
 	public List<User> BFS(){
 		List<User> result = new ArrayList<User>();
-		//result.add(thisUser);
+		
 		Set<User> visited = new HashSet<User>();
 		
 		Queue<User> queue = new LinkedList<User>();
@@ -189,7 +208,9 @@ abstract public class MiddleClass extends LowerClass{
 			
 			result.add(element);
 			
+			if (this.userGraph.containsKey(element) && !this.userGraph.get(element).isEmpty()){
 			Set<User> adjacency = this.userGraph.get(element);
+			
 			
 			for (User a: adjacency){
 				if (!visited.contains(a)){
@@ -198,15 +219,11 @@ abstract public class MiddleClass extends LowerClass{
 				}
 				
 			}
+			}
 			
 			
 		}
 		
-//		Set<User> keyset = this.userGraph.keySet();
-//		for (User s: keyset){
-//			if (!result.contains(s))
-//				this.userGraph.remove(s);
-//		}
 		Log.i("BFS", ""+result.size());
 		
 		return result;
@@ -217,7 +234,7 @@ abstract public class MiddleClass extends LowerClass{
 
 	@Override
 	void onReceivingData(Data data) {
-		// TODO Auto-generated method stub
+		// check uuid, do not analyze msgs appear before
 		if (checkUUID(data.getUUID())) {
 			Log.i(""+data.getType(), "Received before");
 			return;
@@ -225,18 +242,19 @@ abstract public class MiddleClass extends LowerClass{
 		else if (data.getType() == Data.TEXT){
 			
 			Log.i(""+data.getType(), "uuid: "+data.getUUID());
+			// if is receiver
 			if (data.getReceiver().equals(this.thisUser) ){
 				Message msg = new Message(data.getSender(), data.getReceiver(), ((TextData)data).getText());
 				this.onMessage(msg);
 				Log.i("MiddleClass", ""+((TextData)data).getText());
 
 			}else if (!((TextData)data).checkFootprint(thisUser)){
+				// if is not the receiver
 				Set<String> availableDevices = this.getAllConnectedDevices();
 				for (String s: availableDevices)
 					try {
 						this.sendData( s, serialize(data));
 					} catch (IOException e) {
-						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
 				Log.i("MiddleClass", ""+"this is for" + data.getReceiver());
@@ -247,17 +265,17 @@ abstract public class MiddleClass extends LowerClass{
 		
 		
 		else if (data.getType() == Data.TOPOLOGY){
-			Log.i("TopoMsg","Received");
+			Log.i("TopoMsg","this is from " + data.getSender().getUserName());
 			
-			// LS
+			// Link state
 			if (!((TopologyData)data).checkFootprint(thisUser)){
 				// step 1: check if same. if not, update, and call onNewUser
 				if (this.userGraph.get(thisUser).contains(data.getSender())){
-					
+					// case 1: if sender not exists
 					if (!this.userGraph.containsKey(data.getSender())
 						||	!this.userGraph.get(data.getSender()).equals(((TopologyData)data).getDirectNeighbors())){
 						this.userGraph.put(data.getSender(), ((TopologyData)data).getDirectNeighbors());
-						
+						Log.i("case1"," ");
 						List<User> newUsers = BFS();
 						newUsers.remove(this.thisUser);
 						Log.i("newUser#: ", ""+newUsers.size());
@@ -267,7 +285,6 @@ abstract public class MiddleClass extends LowerClass{
 							try {
 								this.sendData( s, serialize(data));
 							} catch (IOException e) {
-								// TODO Auto-generated catch block
 								e.printStackTrace();
 							}
 					}
@@ -276,9 +293,9 @@ abstract public class MiddleClass extends LowerClass{
 					
 					
 				}else{
-					//this.userGraph.get(thisUser).add(data.getSender());
+					// case 2: if the sender is not within the usergraph
 					this.userGraph.put(data.getSender(), ((TopologyData)data).getDirectNeighbors());
-					
+					Log.i("case2"," ");
 					List<User> newUsers = BFS();
 					newUsers.remove(this.thisUser);
 					this.onNewUser(newUsers);
@@ -288,7 +305,6 @@ abstract public class MiddleClass extends LowerClass{
 						try {
 							this.sendData( s, serialize(data));
 						} catch (IOException e) {
-							// TODO Auto-generated catch block
 							e.printStackTrace();
 						}
 					
@@ -308,8 +324,12 @@ abstract public class MiddleClass extends LowerClass{
 		
 		else if (data.getType() == Data.ADDNEIGHBOR){
 			// update neighbors
+			// only need to broadcast this msg, because an addNeighborMsg will not update thisUser's
+			// topology
 			
 			if (!((AddNeighborData)data).checkFootprint(thisUser)){	
+			
+				Log.i("AddNewNeighbor", "this is from " + data.getSender().getUserName());
 				Data topoData = new TopologyData(this.thisUser, null, this.userGraph.get(thisUser));
 			
 				Set<String> availableDevices = this.getAllConnectedDevices();
@@ -339,9 +359,10 @@ abstract public class MiddleClass extends LowerClass{
 			if (!((RemoveNeighborData)data).checkFootprint(thisUser)){
 			
 				
-				
-				this.userGraph.get(userToRemove1).remove(userToRemove2);
-				this.userGraph.get(userToRemove2).remove(userToRemove1);
+				if (this.userGraph.get(userToRemove1).contains(userToRemove2))
+					this.userGraph.get(userToRemove1).remove(userToRemove2);
+				if (this.userGraph.get(userToRemove2).contains(userToRemove1))
+					this.userGraph.get(userToRemove2).remove(userToRemove1);
 				
 				List<User> usersContained = BFS();
 				Set<User> allUsers = this.userGraph.keySet();
